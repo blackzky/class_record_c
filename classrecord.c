3ae9eededfcd972d2854b5712d25dae293b48abf
Gervel Giva <gervelgiva@gmail.com>
2013-03-16 20:35:46 +0800

    shows list of students, trapped if size is zero

--- classrecord.c
+++ classrecord.c
@@ -48,11 +48,11 @@ void displayOptions(char *options[], int start, int end);
 //student module: -----------------------------------------
 void	showStudentOptions(int size);
 void handleStudentMenuInput();
-void printStudent(Student s);
+void printStudent(Student s, int select_id);
 Student createStudent();
 
 //doubly linked list module:-------------------------------
-void initNode(Node *head, Node *tail);
+void initNode(Node *head);
 void addStudent(Node *pointer, Student data); 
 Student findStudent(Node *pointer, int key); 
 void deleteStudent(Node *pointer, int id); 
@@ -64,9 +64,10 @@ char *getSelector(int id){ return ( (SELECTOR == id) ? ">>" : "  "); }
 
 int cycleInput(int size){
 	int INPUT = getch();	
-	if(INPUT == UP_KEY)		SELECTOR = (SELECTOR <= 0) ? (size-1) : (SELECTOR - 1); 
-	else if(INPUT == DOWN_KEY)	SELECTOR = (SELECTOR + 1) % size; 
-
+     if(size > 0){
+          if(INPUT == UP_KEY)		SELECTOR = (SELECTOR <= 0) ? (size-1) : (SELECTOR - 1); 
+          else if(INPUT == DOWN_KEY)	SELECTOR = (SELECTOR + 1) % size; 
+     }
 	return (INPUT == ENTER_KEY) ? ENTER_KEY : -1;
 }
 
@@ -79,12 +80,13 @@ void displayOptions(char *options[], int start, int end){
 /* END HELPERS FUNCTIONS -------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------*/
 /* START DOUBLY LINKEDLIST FUNCTIONS -------------------------------------------------*/
-void initNode(Node *student, Node *tail){
+void initNode(Node *student){
+     Node *tail;
+     Student s = {-1, "", "", 0, 0, 0, 0, 0};
      student = (Node *)malloc(sizeof(Node)); 
      tail = student;
      tail->next = NULL;
      tail->prev = NULL;
-     Student s = {-1, "", "", 0, 0, 0, 0, 0};
      student->data = s;
 }
 void addStudent(Node *pointer, Student data) {
@@ -147,16 +149,16 @@ void	showStudentOptions(int size){
 	printf("\t   (Use the arrow keys to navigate and press enter to select)\n\n");
      displayOptions(STUDENT_MENU, start, end);
 }
-
-void printStudent(Student s){
-     printf("ID: %d", s.id);
-     printf("\nFirstname: %s", s.first_name);
-     printf("\nLastname: %s", s.last_name);
-     printf("\nQuiz: %d", s.quiz);
-     printf("\nHW: %d", s.hw);
-     printf("\nAttendance: %d", s.attendance);
-     printf("\nFinal exam: %d", s.final_exam);
-     printf("\nGrade: %d\n", s.grade);
+void printStudent(Student s, int select_id){
+     printf("\n   %s", getSelector(select_id));
+     printf("\t[ID: %d]", s.id);
+     printf("\n\tName: %s", s.first_name);
+     printf(" %s", s.last_name);
+     printf("\n\tScores: %d", s.quiz);
+     printf(" %d", s.hw);
+     printf(" %d", s.attendance);
+     printf(" %d", s.final_exam);
+     printf("\n\tGrade: %d\n\n", s.grade);
 }
 
 Student createStudent(){
@@ -169,25 +171,31 @@ Student createStudent(){
 
 void printStudentList(Node *pointer){
      int size = getSize(students); 
-     printf("Size of list: %d\n", size);
+     int i = 0;
+     printf("\n\t\t\t\tList of students:");
      if(size == 0){
-          printf("\nThe list is empty.\n");
+          printf("\n\n\t\t\t\tThe list is empty.\n");
      }else{
-          printf("\nThe list is not empty\n");
+          while(pointer->next!=NULL) {
+               pointer = pointer -> next;
+               printStudent(pointer->data, i++);
+          }
      }
 
 }
 /* Input handler (Students) */
 void handleStudentMenuInput(){
      int size  = 5;
-     Student temp = {5, "ab", "cd", 1, 2, 3, 4, 5};
+     Student temp = {5, "Gadi", "Galindo", 1, 2, 3, 4, 5};
      do{ showStudentOptions(size); }while(cycleInput(size)!= ENTER_KEY);
 
      switch(SELECTOR){
-          case 0: system("cls");
-                  printStudentList(students);
-                  getch();
-                  break;
+          case 0: 
+               do{  
+                    system("cls");
+                    printStudentList(students);
+               }while(cycleInput(getSize(students)) != ENTER_KEY);
+               break;
           case 1:
                   addStudent(students, temp);
                   break;
@@ -205,14 +213,14 @@ void handleState(){
 }
 
 int main(){
+     //initNode(students); 
      Node *tail;
-     Student dummy = {-1, "empty", "empty", 0, 0, 0, 0, 0};
-     //initNode(students, tail); 
+     Student s = {-1, "", "", 0, 0, 0, 0, 0};
      students = (Node *)malloc(sizeof(Node)); 
      tail = students;
      tail->next = NULL;
      tail->prev = NULL;
-     tail->data = dummy;
+     students->data = s;
 
      handleState();
 	getch();
